5184e4b545a0de1695f310b3984085ca3b372a2a
Flannon Jackson <flannon@nyu.edu>
2018-12-01 20:19:37 -0500

    validate again

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from subprocess import call
 from subprocess import check_output
 
+import argparse
 import boto3
 import json
 import os
@@ -53,22 +54,35 @@ def get_sha():
     sha = '11111111'
     return sha
 
-def main():
+def get_ssh_user(build_os):
+    if build_os == 'centos':
+        ami_ssh_user = build_os
+    elif build_os == 'rhel':
+        ami_ssh_user = 'ec2-user'
+    elif build_os == 'fedora':
+        ami_ssh_user = build_os
+
+    return ami_ssh_user
+
+
+def build_config(build_os):
     print("test")
-    #access = os.environ['AWS_ACCESS_KEY_ID']
-    #print("access: " + access)
+    access = os.environ['AWS_ACCESS_KEY_ID']
+    print("access: " + access)
     ami_instance_profile = os.environ['AMI_INSTANCE_PROFILE']
-    #print("ami_instance_profile: " + ami_instance_profile)
+    print("ami_instance_profile: " + ami_instance_profile)
     print("AMI_INSTANCE_PROFILE: " + os.environ['AMI_INSTANCE_PROFILE'])
 
     dirname = os.path.dirname(os.path.realpath(sys.argv[0]))
-    os_name = sys.argv[1]
-    image_spec = get_image_spec(os_name)
+
+    ##os_name = sys.argv[1]
+    print("build_os: " + build_os)
+    image_spec = get_image_spec(build_os)
     caller_id = image_spec[0]
     base_image_prefix = image_spec[1]
-    #print("is_public, image_spec[2]: " + image_spec[2])
+    print("is_public, image_spec[2]: " + image_spec[2])
     is_public = image_spec[2]
-    #print("is_public: " + is_public)
+    print("is_public: " + is_public)
     base_ami_id, base_name = get_base_ami(caller_id, base_image_prefix, is_public)
     print("base_ami_id: " + base_ami_id)
     os.environ['BASE_AMI_ID'] = base_ami_id
@@ -79,31 +93,74 @@ def main():
     ami_name = base_name[:50]
     #ami_name = base_name
 
-    ami_ssh_user = 'centos'
+    #ami_ssh_user = 'centos'
+    ami_ssh_user = get_ssh_user(build_os)
+
     os.environ['AMI_SSH_USER'] = ami_ssh_user
-    sha = get_sha()
 
-    build_config = "base/" + ami_ssh_user + "-base.json"
-    v = check_output(["packer", "validate", build_config])
+    #bc = "-var-file=" + dirname + "/base/var/" + ami_ssh_user + "-base.json " + dirname + "base/" + ami_ssh_user + "-base.json"
+    bc = "base/" + ami_ssh_user + "-base.json"
 
-    print("Config vars")
-    print("BASE_NAME: " + os.environ['BASE_NAME'])
-    print("BASE_AMI_ID: " + os.environ['BASE_AMI_ID'])
-    print("SHA: " + os.environ['SHA'])
-    print("AMI_SSH_USER: " + os.environ['AMI_SSH_USER'])
+    return bc
 
-    #print("v:" + v + ':')
-    if v == "Template validated successfully.\n":
-        print("building ami")
-        #call(["packer", "build", build_config])
-    else:
-        print("Template validation falied")
-        print(v)
 
-    test = dirname + '/test.sh'
-    #call([test])
-    #print('ami_name: ' + ami_name)
+def main():
+#
 
+    ##### !+
+    ##v = check_output(["packer", "validate", build_config])
+
+    ###print("v:" + v + ':')
+    ##if v == "Template validated successfully.\n":
+    ##    print("Template validatd successfully.")
+    ##    print("building ami")
+    ##    #call(["packer", "build", build_config])
+    ##else:
+    ##    print("Template validation falied")
+    ##    print(v)
+    ##### !-
+#
+    ##test = dirname + '/test.sh'
+    ##call([test])
+    ##print('ami_name: ' + ami_name)
+
+    ap = argparse.ArgumentParser(description='Build images')
+    ap.add_argument("-v", "--validate", action='store_true',
+                    help='validate only')
+    ap.add_argument("-o", "--os", nargs='*', required=True,
+                    help='name of the base os')
+    args = vars(ap.parse_args())
+
+    #print("Hi there {}, it's nice to build you.".format(args["osname"]))
+#
+#    print(args["osname"])
+
+    if args["validate"]:
+        print("validating template " + str(args["validate"]))
+        print("validate only")
+
+        #build_os = args(["os"])
+        #print(build_os)
+
+        build_os = args["os"][0]
+        bc = build_config(build_os)
+
+        print("Config vars")
+        print("BASE_NAME: " + os.environ['BASE_NAME'])
+        print("BASE_AMI_ID: " + os.environ['BASE_AMI_ID'])
+        print("SHA: " + os.environ['SHA'])
+        print("AMI_SSH_USER: " + os.environ['AMI_SSH_USER'])
+
+        #call(["packer", "validate", vbc, bc])
+        print("packer validate " + bc)
+
+    elif args["os"]:
+        print("building ami...")
+        print(args["os"])
+        build_config(args["os"])
+        print("building")
+    #else:
+    #    print("Usage: build.py -os <operating sytem> || -v")
 
 
 if __name__ == "__main__":
